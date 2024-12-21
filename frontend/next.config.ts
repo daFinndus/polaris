f88e0c3cc1bed1d3d11f30547d60fd3c1739a7d9
daFinndus <finnlucajensen555@gmail.com>
2024-12-21 15:42:29 +0100

    feat: Add x robot tag header

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,20 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
+    async headers() {
+        const headers = [];
+        headers.push({
+            headers: [
+                {
+                    key: 'X-Robots-Tag',
+                    value: 'index',
+                },
+            ],
+            source: '/:path*',
+        });
+        
+        return headers;
+    },
 };
 
 export default nextConfig;
