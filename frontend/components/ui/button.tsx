3b1cfe948a36718bebc71a70dceed3c8a300d43a
daFinndus <finnlucajensen@proton.me>
2025-04-15 00:03:27 +0200

    chore: Update button component

--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
         variants: {
             variant: {
                 default: "bg-primary text-primary-foreground shadow hover:bg-primary/90",
-                color: "bg-color text-color-lighter shadow-sm hover:bg-color/80",
+                color: "bg-color text-color-lightest shadow-sm hover:bg-color/80",
                 destructive: "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
                 outline: "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
                 secondary: "bg-secondary text-secondary-foreground shadow-sm hover:bg-background-lightest",
